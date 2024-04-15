a5944ed56b7a3ab7d8d23f02c0cd6729af98d098
Purva Agarwal <purva98@gmail.com>
2024-04-14 21:14:12 -0400

    modified:   App.js 	new file:   Kanbas/users/dao.js 	new file:   Kanbas/users/model.js 	new file:   Kanbas/users/routes.js 	new file:   Kanbas/users/schema.js 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 import express from 'express'
+import mongoose from "mongoose"
 import Hello from "./Hello.js"
 import Lab5 from './Lab5.js'
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentsRoutes from './Kanbas/assignments/routes.js';
-
-
+import UserRoutes from './Kanbas/users/routes.js'
+import session from "express-session";
+import "dotenv/config";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express()
-app.use(cors())
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+  );
+  const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+}
+app.use(session(sessionOptions));  
 app.use(express.json());
 Hello(app)
 Lab5(app)
 CourseRoutes(app)
 ModuleRoutes(app)
 AssignmentsRoutes(app)
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
